35c06b1f3b24e921bc2b575f59cc0071b24c2f29
GerardoUM <gerardojur@gmail.com>
2023-02-17 18:12:05 -0500

    agregar estilos tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="bg-red-300">Hola mundo tailwind</div>
+
+      <div className="flex flex-col items-center justify-center">
+        <div className="mx-2 bg-green-500">Elemento 1</div>
+        <div className="mx-2 bg-red-800">Elemento 2</div>
+        <div className="mx-2 bg-purple-400">Elemento 3</div>
+        <div className="mx-2 bg-yellow-700">Elemento 4</div>
+        <button className="border-2 border-blue-900 p-4 bg-green-200 text-gray-700 hover:bg-pink-500 rounded-lg shadow-lg">
+          Haz clic aquí
+        </button>
+        <button className="border-2 border-blue-900 p-4 bg-green-200 text-gray-700 hover:bg-pink-500 rounded-lg shadow-lg">
+          Este es el botón 2
+        </button>
+      </div>
     </div>
   );
 }
